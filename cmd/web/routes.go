1442a3455bd35255f8de3799742897c011f421da
rxrzzz <temiloluwagithub@gmail.com>
2024-09-29 07:25:46 +0100

    simplified middleware with justinas/alice package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("../../ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
@@ -10,5 +14,6 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	return standard.Then(mux)
 }
